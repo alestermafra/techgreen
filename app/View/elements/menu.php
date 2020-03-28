a87455103cb2838291b08bc5bd29f78a41732c3f
Alester Mafra <reetsr2@gmail.com>
2020-03-28 11:54:53 -0300

    Alterações na ordem do menu.

--- app/View/elements/menu.php
+++ app/View/elements/menu.php
@@ -1,4 +1,98 @@
 <ul class="menu nav flex-column bg-dark">
+	<a href="<?= $this->url('/agenda/agenda') ?>" style="text-decoration: none;">
+		<li class="item-menu nav-item text-light noselect">Agenda</li>
+	</a>
+	<a href="<?= $this->url('/painel/pf') ?>" style="text-decoration: none;">
+		<li class="item-menu nav-item text-light noselect">Velejadores</li>
+	</a>
+	<a href="#" style="text-decoration: none;">
+		<li class="item-menu nav-item text-light noselect">Brechó</li>
+	</a>
+	<li class="nav-item text-light noselect">
+		<div class="item-menu noselect" data-toggle="collapse" data-target="#menu-administracao">Administração</div>
+		<div class="collapse" id="menu-administracao">
+			<ul class="menu nav flex-column bg-dark">
+				<a href="<?= $this->url('/fornecedor') ?>" style="text-decoration: none;">
+					<li class="item-menu nav-item text-light noselect" style="padding-left: 25px;">Fornecedores</li>
+				</a>
+				<a href="<?= $this->url('/colaboradores') ?>" style="text-decoration: none;">
+					<li class="item-menu nav-item text-light noselect" style="padding-left: 25px;">Colaboradores</li>
+				</a>
+			</ul>
+		</div>
+	</li>
+	<li class="nav-item text-light noselect">
+		<div class="item-menu noselect" data-toggle="collapse" data-target="#menu-servicos">Serviços</div>
+		<div class="collapse" id="menu-servicos">
+			<ul class="menu nav flex-column bg-dark">
+				<a href="<?= $this->url('/guardaria') ?>" style="text-decoration: none;">
+					<li class="item-menu nav-item text-light noselect" style="padding-left: 25px;">Guarderia</li>
+				</a>
+				<a href="<?= $this->url('/aula') ?>" style="text-decoration: none;">
+					<li class="item-menu nav-item text-light noselect" style="padding-left: 25px;">Aulas</li>
+				</a>
+				<a href="<?= $this->url('/locacao') ?>" style="text-decoration: none;">
+					<li class="item-menu nav-item text-light noselect" style="padding-left: 25px;">Locação</li>
+				</a>
+				<a href="<?= $this->url('/diariavelejador') ?>" style="text-decoration: none;">
+					<li class="item-menu nav-item text-light noselect" style="padding-left: 25px;">Diária/Pernoite</li>
+				</a>
+			</ul>
+		</div>
+	</li>
+	<li class="nav-item text-light noselect">
+		<div class="item-menu noselect" data-toggle="collapse" data-target="#menu-tabela-de-precos">Tabela de Preços</div>
+		<div class="collapse" id="menu-tabela-de-precos">
+			<ul class="menu nav flex-column bg-dark">
+				<a href="<?= $this->url('/servicos/precos') ?>" style="text-decoration: none;">
+					<li class="item-menu nav-item text-light noselect" style="padding-left: 25px;">Serviços</li>
+				</a>
+			</ul>
+		</div>
+	</li>
+	<li class="nav-item text-light noselect">
+		<div class="item-menu noselect" data-toggle="collapse" data-target="#menu-relatorios">Relatórios</div>
+		<div class="collapse" id="menu-relatorios">
+			<ul class="menu nav flex-column bg-dark">
+				<a href="<?= $this->url('/guardaria/relatorio') ?>" style="text-decoration: none;">
+					<li class="item-menu nav-item text-light noselect" style="padding-left: 25px;">Guarderia</li>
+				</a>
+				<a href="<?= $this->url('/aula/relatorio') ?>" style="text-decoration: none;">
+					<li class="item-menu nav-item text-light noselect" style="padding-left: 25px;">Aulas</li>
+				</a>
+				<a href="<?= $this->url('/locacao/relatorio') ?>" style="text-decoration: none;">
+					<li class="item-menu nav-item text-light noselect" style="padding-left: 25px;">Locação</li>
+				</a>
+				<a href="<?= $this->url('/diariavelejador/relatorio') ?>" style="text-decoration: none;">
+					<li class="item-menu nav-item text-light noselect" style="padding-left: 25px;">Diária/Pernoite</li>
+				</a>
+				<a href="<?= $this->url('/ocorrencia') ?>" style="text-decoration: none;">
+					<li class="item-menu nav-item text-light noselect" style="padding-left: 25px;">Ocorrências</li>
+				</a>
+				<a href="<?= $this->url('/painel/relatorio') ?>" style="text-decoration: none;">
+					<li class="item-menu nav-item text-light noselect" style="padding-left: 25px;">Velejadores</li>
+				</a>
+				<a href="<?= $this->url('/painel/relatorio_aniversariantes') ?>" style="text-decoration: none;">
+					<li class="item-menu nav-item text-light noselect" style="padding-left: 25px;">Aniversariantes</li>
+				</a>
+			</ul>
+		</div>
+	</li>
+	<?php if(Auth::user()['lg'] === 'gold'): ?>
+		<a href="<?= $this->url('/usuario') ?>" style="text-decoration: none;">
+			<li class="item-menu nav-item text-light noselect">Usuários</li>
+		</a>
+	<?php endif; ?>
+	<li class="nav-item mt-5 mb-3 text-muted" style="padding-left: 10px;" title="M.Gold®">
+		Powered by M.Gold®
+	</li>
+</ul>
+
+<!--
+
+	COMO ERA O MENU ANTERIOR : PEGANDO DO  BANCO
+
+<ul class="menu nav flex-column bg-dark">
 <?php foreach($menu as $m): ?>
 	<li class="nav-item text-light">
 		<div class="item-menu noselect" data-toggle="collapse" data-target=".collapse-menu-<?php echo $m['cmenu'] ?>"><?php echo $m['nmenu'] ?></div>
@@ -15,10 +109,9 @@
 		</div>
 	</li>
 <?php endforeach ?>
-	<li class="nav-item mt-5 mb-3 text-muted" style="padding-left: 10px;" title="M.Gold®">
-		Powered by M.Gold®
-	</li>
 </ul>
+
+-->
 
 <style>
 	.noselect {
